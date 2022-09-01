5e211d20133abe7c7557596cdce4d6e7d6687796
fraanma <110063099+fraanma@users.noreply.github.com>
2022-09-01 18:25:36 -0300

    Encriptador ONE con botones y colores

---
+++ script.js
@@ -0,0 +1,52 @@
+function encriptar(){
+    var texto = document.getElementById("text-input").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    //i es para que tome tanto mayus como minuscula
+    //g sirve para toda la linea u oracion 
+    //m es para que afecte a multiples lineas o parrafos
+
+
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("imgder").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+
+}
+
+
+function desencriptar(){
+    var texto = document.getElementById("text-input").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    //i es para que tome tanto mayus como minuscula
+    //g sirve para toda la linea u oracion 
+    //m es para que afecte a multiples lineas o parrafos
+
+
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imgder").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+    
+}
+
+function copiar() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy");
+    alert("Se copio");
+    
+}
